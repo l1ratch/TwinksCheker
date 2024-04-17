52afe13633133a3c3303e88922898c72f9b76c10
Линк <sm171105@bk.ru>
2024-04-17 15:13:27 +0300

    Initial commit

---
+++ src/main/java/l1ratch/twinkscheker/TwinksCheker.java
@@ -0,0 +1,17 @@
+package l1ratch.twinkscheker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TwinksCheker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
